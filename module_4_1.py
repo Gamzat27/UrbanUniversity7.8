0ba6e9cae88193d822898f7bf4bccec064cf6757
Gamzat27 <gamzat27@icloud.com>
2024-12-20 21:00:47 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as di
+from true_math import divide as dv
+
+result = di(10,0)
+result1 = di(25,5)
+result2 = di(24.4,4.4)
+print(result)
+print(result1)
+print(result2)
+result3 = dv(14,0)
+result4 = dv(26,4)
+result5 = dv(87.4,9.2)
+print(result3)
+print(result4)
+print(result5)
+
+
